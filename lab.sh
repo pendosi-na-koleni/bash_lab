d9ad837779b33e37fbe7e92027f696b62303448a
Uliana <124802136+UlianaTheBesd@users.noreply.github.com>
2025-10-07 21:05:06 +0300

    Some checks lab.sh

--- lab.sh
+++ lab.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 path="/home/mafaka/arcpr/prac/"
 perc=20
-l="${1-:path}"
-n="${2-:perc}"
+l="${1-:$path}" # добавила $ к пути (было просто path).
+n="${2-:$perc}" # здесь тоже +$
 #if no input after executing choosing params by default
 #start test.sh
 if ! bash test.sh "$l" "$n"; then
@@ -11,8 +11,8 @@ exit 1
 fi
 cur_dir=$(du -s "$l" | cut -f1)
 home_dir=$(du -s "/home" | cut -f1)
-res=$(echo "scale=2; $cur_dir/$home_dir" | bc | cut -d. -f2)
-files=($(ls -tr "$l"))
+res=$(echo "scale=2; $cur_dir/$home_dir" | bc | cut -d. -f2) # можно добавить проверку, что число <= 1, или работать через bc: res=$(echo "scale=0; $cur_dir*100/$home_dir" | bc) ?
+files=($(ls -tr "$l")) # если в именах файлов будут спецсимволы - конструкция может сломаться. mapfile -t files < <(ls -tr -- "$l") ?
 to_archivate=()
 mkdir -p "backup"
 for ((i=0; i<${#files[@]};i++))
