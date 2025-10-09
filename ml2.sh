a31d5b007ceecc2a30c2cf0654f4f3674db224bc
mazafaka <93437310+pendosi-na-koleni@users.noreply.github.com>
2025-10-09 16:24:31 +0300

    Update ml2.sh

--- ml2.sh
+++ ml2.sh
@@ -18,7 +18,7 @@ fi
 total_size=$(du -s "/home" | cut -f1)
 dir_size=$(du -s "$dir" | cut -f1) 
 current_usage=$(echo "scale=7; $dir_size * 100 / $total_size" | bc -l)
-echo "usage: $(printf "%.2f" "$current_usage")%"
+echo "usage: $(echo "scale=2; $current_usage/1" | bc)%"
 if (( $(echo "$current_usage <= $threshold" | bc -l) )); then
     echo "No need for archivating"
     exit 0
@@ -38,7 +38,7 @@ for (( i=0; i<${#files[@]}; i++ )); do
     archived_size=$((archived_size + file_size))
     new_usage=$(echo "scale=7; ($dir_size - $archived_size) * 100 / $total_size" | bc -l)
     if (( $(echo "$new_usage <= $threshold" | bc -l) )); then
-        echo "Target usage reached: $(printf "%.2f" "$new_usage")%"
+        echo "Target usage reached: $(echo "scale=2; $new_usage/1" | bc)%"
         break
     fi
 done
