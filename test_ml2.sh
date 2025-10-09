aa26275bafab2abd3a89df5219a9943ac489a500
Uliana <124802136+UlianaTheBesd@users.noreply.github.com>
2025-10-09 17:57:52 +0300

    Added == test_ml2.sh

--- test_ml2.sh
+++ test_ml2.sh
@@ -13,37 +13,35 @@ touch "$dir/file1.txt" "$dir/file2.txt" # + создаём 2 файла
 
 # test 1 (папки нет).
 echo "test 1: directory does not exist."
-echo "================================="
 ./ml2.sh "$dir_not_exist" 10
-echo "================================="
 echo -e "Test 1 completed\n"
+
+echo "================================="
 
 # test 2 (процент указан не число).
 echo "test 2: threshold is not numeric."
-echo "================================="
 ./ml2.sh "$dir" "opps I did it again"
-echo "================================="
 echo -e "Test 2 completed\n"
+
+echo "================================="
 
 # test 3 (процент не соотв. запросу).
 echo "test 3: threshold is not in range (0,100)."
-echo "================================="
 ./ml2.sh "$dir" 101
-echo "================================="
 echo -e "Test 3 completed\n"
+
+echo "================================="
 
 # test 4 (папка пустая).
 echo "test 4: directory is empty."
-echo "================================="
 ./ml2.sh "$dir_empty" 10
-echo "================================="
 echo -e "Test 4 completed\n"
+
+echo "================================="
 
 # test 5 (успешный запуск кода).
 echo "test 5: everything is fine."
-echo "================================="
 ./ml2.sh "$dir" 0.01
-echo "================================="
 echo -e "Test 5 completed\n"
 
 rm -rf "$dir_empty" "$backup"
